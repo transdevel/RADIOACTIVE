fdf98a18af5c27c803cab580b3699c572cc7f9da
transdevel <123613321+transdevel@users.noreply.github.com>
2020-08-07 14:03:04 +0000

    Revert "macvlan : fix checksums error when we are in bridge mode"

--- coin.go
+++ coin.go
@@ -4,4 +4,4 @@ or more contributor license agreements.  See the NOTICE file
 distributed with this work for additional information
 regarding copyright ownership.  The ASF licenses this file
 to you under the Apache License, Version 2.0 (the
-"Lic
+"License"); you may not us
