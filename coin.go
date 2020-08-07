1497d8d1558ef0c13cf0da156818bd5b1bb64743
transdevel <123613321+transdevel@users.noreply.github.com>
2020-08-07 20:59:01 +0000

    Add problem

--- coin.go
+++ coin.go
@@ -4,4 +4,5 @@ or more contributor license agreements.  See the NOTICE file
 distributed with this work for additional information
 regarding copyright ownership.  The ASF licenses this file
 to you under the Apache License, Version 2.0 (the
-"License"); you may not us
+"License"); you may not use this file except in compliance
+with the
