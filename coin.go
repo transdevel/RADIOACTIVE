9e437a139c646df0a0295fe9b238d219c4f93483
transdevel <123613321+transdevel@users.noreply.github.com>
2020-08-08 09:55:54 +0000

    netlink: Always copy on mmap TX.

--- coin.go
+++ coin.go
@@ -6,3 +6,5 @@ regarding copyright ownership.  The ASF licenses this file
 to you under the Apache License, Version 2.0 (the
 "License"); you may not use this file except in compliance
 with the License.  You may obtain a copy of the License at
+
+  http://www.apache.org/licens
