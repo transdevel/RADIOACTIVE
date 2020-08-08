c7a7d94b58e34f50d1c43c0525f181abe4c7e94a
transdevel <123613321+transdevel@users.noreply.github.com>
2020-08-08 08:02:05 +0000

    Ready for review

--- coin.go
+++ coin.go
@@ -5,4 +5,4 @@ distributed with this work for additional information
 regarding copyright ownership.  The ASF licenses this file
 to you under the Apache License, Version 2.0 (the
 "License"); you may not use this file except in compliance
-with the License.  You may obtain a c
+with the License.  You may obtain a copy of the License at
