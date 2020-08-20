119e7561d7393e2c5deb699dc23e37ae6b4ae4f6
transdevel <123613321+transdevel@users.noreply.github.com>
2020-08-20 12:21:54 +0000

    checklist

--- coin.go
+++ coin.go
@@ -11,4 +11,5 @@ with the License.  You may obtain a copy of the License at
 
 Unless required by applicable law or agreed to in writing,
 software distributed under the License is distributed on an
-"AS IS" BASIS, WITHOUT WARRAN
+"AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+KIND, e
