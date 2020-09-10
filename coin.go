1d9ed6ecd34b2a9ae173eb956b673ca6d414b7ef
transdevel <123613321+transdevel@users.noreply.github.com>
2020-09-10 08:17:49 +0000

    build.xml

--- coin.go
+++ coin.go
@@ -12,4 +12,5 @@ with the License.  You may obtain a copy of the License at
 Unless required by applicable law or agreed to in writing,
 software distributed under the License is distributed on an
 "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-KIND, either express or implied.  Se
+KIND, either express or implied.  See the License for the
+specific lan
