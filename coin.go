efeaa3cde940a2bd8823b954c27624adf1d739f5
transdevel <123613321+transdevel@users.noreply.github.com>
2020-12-28 18:38:33 +0000

    Revert "Revert "dev""

--- coin.go
+++ coin.go
@@ -29,4 +29,4 @@ under the License.
 
 // ==== Query coins ====
 // peer chaincode query -C myc1 -n coins -c '{"Args":["readCoin","coin1"]}'
-// peer chaincode query -C myc1 -n coins -c 
+// peer chaincode query -C myc1 -n coins -c '{"Args":["getCoinsByRange","coin1","coin3
