cd2c6b6d85ece1b14e07222078c8b0d03224a8de
transdevel <123613321+transdevel@users.noreply.github.com>
2020-12-30 20:45:23 +0000

    perfil

--- coin.go
+++ coin.go
@@ -34,4 +34,4 @@ under the License.
 
 // Rich Query (Only supported if CouchDB is used as state database):
 //   peer chaincode query -C myc1 -n coins -c '{"Args":["queryCoinsByOwner","tom"]}'
-//   peer chaincode query -C myc1 -n coins -c '{
+//   peer chaincode query -C myc1 -n coins -c '{"Args":["queryCoins","{\"selector
