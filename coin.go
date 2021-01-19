b6bca0597bf9fdc9b5bab40ddf38ee674e68cea8
transdevel <123613321+transdevel@users.noreply.github.com>
2021-01-19 14:39:14 +0000

    tweak ui

--- coin.go
+++ coin.go
@@ -40,4 +40,4 @@ under the License.
 //
 // Indexes in CouchDB are required in order to make JSON queries efficient and are required for
 // any JSON query with a sort. As of Hyperledger Fabric 1.1, indexes may be packaged alongside
-// chaincode in a MET
+// chaincode in a META-INF/statedb/couchdb/indexes directory.
