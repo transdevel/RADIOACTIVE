0b356be9373393ee954fdcc8d99eaa2f4876578f
transdevel <123613321+transdevel@users.noreply.github.com>
2021-03-19 17:44:05 +0000

    Rename License to LICENSE

--- coin.go
+++ coin.go
@@ -55,4 +55,4 @@ under the License.
 // If you have access to the your peer's CouchDB state database in a development environment,
 // you may want to iteratively test various indexes in support of your chaincode queries.  You
 // can use the CouchDB Fauxton interface or a command line curl utility to create and update
-// indexes. Then once you finalize an index, i
+// indexes. Then once you finalize an index, include the index definition along
