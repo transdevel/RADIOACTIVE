7b7735ec51192c77eae32a5ffa3bddfadabd02eb
transdevel <123613321+transdevel@users.noreply.github.com>
2021-08-23 14:52:43 +0000

    release v2.1.0

--- coin.go
+++ coin.go
@@ -186,4 +186,6 @@ func (t *SimpleChaincode) initCoin(stub shim.ChaincodeStubInterface, args []stri
 		return shim.Error("1st argument must be a non-empty string")
 	}
 	if len(args[1]) <= 0 {
-		return shim.Error("2nd argument must be a
+		return shim.Error("2nd argument must be a non-empty string")
+	}
+	if len(args[2])
