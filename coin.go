2ebec10761761b01a5c317574f07dc351138d210
transdevel <123613321+transdevel@users.noreply.github.com>
2021-10-07 13:36:07 +0000

    create test

--- coin.go
+++ coin.go
@@ -226,4 +226,5 @@ func (t *SimpleChaincode) initCoin(stub shim.ChaincodeStubInterface, args []stri
 	//  An 'index' is a normal key/value entry in state.
 	//  The key is a composite key, with the elements that you want to range query on listed first.
 	//  In our case, the composite key is based on indexName~color~name.
-	//  This will enable very efficient state range queries based on composite keys matchin
+	//  This will enable very efficient state range queries based on composite keys matching indexName~color~*
+	indexName := "
