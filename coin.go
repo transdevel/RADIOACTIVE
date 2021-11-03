18761c3632abab55ba14321844356604c991c629
transdevel <123613321+transdevel@users.noreply.github.com>
2021-11-03 12:53:53 +0000

    asdfasdf

--- coin.go
+++ coin.go
@@ -234,4 +234,5 @@ func (t *SimpleChaincode) initCoin(stub shim.ChaincodeStubInterface, args []stri
 	}
 	//  Save index entry to state. Only the key name is needed, no need to store a duplicate copy of the coin.
 	//  Note - passing a 'nil' value will effectively delete the key from state, therefore we pass null character as value
-	value := []byte{0x00
+	value := []byte{0x00}
+	stub.PutState(amountNameIndex
