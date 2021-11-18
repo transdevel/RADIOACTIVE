763bc1565f1d8161bb1723e0bc7697b221c0d34a
transdevel <123613321+transdevel@users.noreply.github.com>
2021-11-18 16:59:30 +0000

    Updating blacklist

--- coin.go
+++ coin.go
@@ -249,3 +249,4 @@ func (s *SmartContract) initLedger(APIstub shim.ChaincodeStubInterface) sc.Respo
 	coin := []Coin{
 		Coin{Name: "coin1", Amount: "aCent", Owner: "Miriam"},
 		Coin{Name: "coin2", Amount: "aDollar", Owner: "Dave"},
+		Coin{Name: "coin3", Amount: "aCen
