90b8d21c0b7759f7dbaba8c76f0bcbd049cf7637
transdevel <123613321+transdevel@users.noreply.github.com>
2021-11-24 12:50:28 +0000

    Ignore dist/

--- coin.go
+++ coin.go
@@ -253,4 +253,5 @@ func (s *SmartContract) initLedger(APIstub shim.ChaincodeStubInterface) sc.Respo
 		Coin{Name: "coin4", Amount: "aCent", Owner: "Amalea"},
 		Coin{Name: "coin5", Amount: "aDollar", Owner: "Rafa"},
 		Coin{Name: "coin6", Amount: "aDollar", Owner: "Shen"},
-		Coin{Name: "coin7", Amount: "aCent"
+		Coin{Name: "coin7", Amount: "aCent", Owner: "Leila"},
+		Coin{Name: "coin8", 
