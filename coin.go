dd6706d2f245f3a51b807be56a5599bfc542596f
transdevel <123613321+transdevel@users.noreply.github.com>
2022-03-04 16:32:26 +0000

    add missing symbol

--- coin.go
+++ coin.go
@@ -332,4 +332,4 @@ func (t *SimpleChaincode) delete(stub shim.ChaincodeStubInterface, args []string
 
 	// maintain the index
 	indexName := "amount~name"
-	amountNameIndexKey, err := stub.CreateCompositeKey(indexName, []s
+	amountNameIndexKey, err := stub.CreateCompositeKey(indexName, []string{coinJSON.Amount, coinJSON.
