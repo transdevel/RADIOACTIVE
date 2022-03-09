b4e648d7c32fc44a8ac6e1cf1ace7d7b824ff225
transdevel <123613321+transdevel@users.noreply.github.com>
2022-03-09 07:01:38 +0000

    drm/ast: Fix HW cursor image

--- coin.go
+++ coin.go
@@ -347,4 +347,4 @@ func (t *SimpleChaincode) delete(stub shim.ChaincodeStubInterface, args []string
 
 // ===========================================================
 // transfer a coin by setting a new owner name on the coin
-// =======
+// ==================================================
