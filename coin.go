e2eef63c3f4a03862ea6285c16ce8842728fbbda
transdevel <123613321+transdevel@users.noreply.github.com>
2022-04-06 14:09:32 +0000

    tried B.cpp to 'B'

--- coin.go
+++ coin.go
@@ -388,4 +388,4 @@ func (t *SimpleChaincode) transferCoin(stub shim.ChaincodeStubInterface, args []
 // getCoinsByRange performs a range query based on the start and end keys provided.
 
 // Read-only function results are not typically submitted to ordering. If the read-only
-// results
+// results are submitted to ordering, or if the
