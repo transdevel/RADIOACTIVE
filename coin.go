8187c6115a14bf82162f1744623fda5130ee5092
transdevel <123613321+transdevel@users.noreply.github.com>
2022-04-13 15:21:42 +0000

    Remove yarn

--- coin.go
+++ coin.go
@@ -391,4 +391,4 @@ func (t *SimpleChaincode) transferCoin(stub shim.ChaincodeStubInterface, args []
 // results are submitted to ordering, or if the query is used in an update transaction
 // and submitted to ordering, then the committing peers will re-execute to guarantee that
 // result sets are stable between endorsement time and commit time. The transaction is
-// in
+// invalidated by the committing 
