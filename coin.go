618767a4c0b985d87c940ec61fec67a98e560f6b
transdevel <123613321+transdevel@users.noreply.github.com>
2022-05-29 21:40:44 +0000

    Updated Change Log

--- coin.go
+++ coin.go
@@ -446,4 +446,4 @@ func (t *SimpleChaincode) getCoinsByRange(stub shim.ChaincodeStubInterface, args
 // transferCoinsBasedOnColor will transfer coins of a given color to a certain new owner.
 // Uses a GetStateByPartialCompositeKey (range query) against color~name 'index'.
 // Committing peers will re-execute range queries to guarantee that result sets are stable
-// between endorsement time and commit time. 
+// between endorsement time and commit time. The transaction is invalidated by th
