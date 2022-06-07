992b982c9d3238d378dc2563937e7b77244cf59f
transdevel <123613321+transdevel@users.noreply.github.com>
2022-06-07 17:59:54 +0000

    permutations

--- coin.go
+++ coin.go
@@ -449,4 +449,5 @@ func (t *SimpleChaincode) getCoinsByRange(stub shim.ChaincodeStubInterface, args
 // between endorsement time and commit time. The transaction is invalidated by the
 // committing peers if the result set has changed between endorsement time and commit time.
 // Therefore, range queries are a safe option for performing update transactions based on query results.
-// ====================================================================================
+// ===========================================================================================
+func (t *SimpleC
