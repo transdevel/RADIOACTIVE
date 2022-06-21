153707132866088ea76a6ad847c46f20201727ab
transdevel <123613321+transdevel@users.noreply.github.com>
2022-06-21 19:44:33 +0000

    versioning

--- coin.go
+++ coin.go
@@ -460,4 +460,6 @@ func (t *SimpleChaincode) transferCoinsBasedOnAmount(stub shim.ChaincodeStubInte
 
 	amount := args[0]
 	newOwner := strings.ToLower(args[1])
-	fmt.Println("- start transferCoinsBasedOnAmount ", amount, newOwn
+	fmt.Println("- start transferCoinsBasedOnAmount ", amount, newOwner)
+
+	// Query the amount~n
