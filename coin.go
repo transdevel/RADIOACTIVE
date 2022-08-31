2c31e4f15646c7f3ac6d7111e20c79fec204370c
transdevel <123613321+transdevel@users.noreply.github.com>
2022-08-31 20:57:21 +0000

    Break long line

--- coin.go
+++ coin.go
@@ -521,4 +521,4 @@ func (t *SimpleChaincode) transferCoinsBasedOnAmount(stub shim.ChaincodeStubInte
 // and accepting a single query parameter (owner).
 // Only available on state databases that support rich query (e.g. CouchDB)
 // =========================================================================================
-func (t *
+func (t *SimpleChaincode) queryCoins
