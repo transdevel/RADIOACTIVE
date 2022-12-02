5a251e75a3a3b2742b29c8aaffc9d248f79ea583
transdevel <123613321+transdevel@users.noreply.github.com>
2022-12-02 08:55:49 +0000

    remove logo

--- coin.go
+++ coin.go
@@ -568,4 +568,4 @@ func (t *SimpleChaincode) queryCoins(stub shim.ChaincodeStubInterface, args []st
 // getQueryResultForQueryString executes the passed in query string.
 // Result set is built and returned as a byte array containing the JSON results.
 // =========================================================================================
-func
+func getQueryResultForQueryString(stub shi
